61542bd90a772e4c95a4b35e109be0c604af6276
EricWRogers <ericwilliamrogers@gmail.com>
2026-02-05 12:36:15 -0600

    Ready for class

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World" << std::endl;
+
+    // random seed
+    
+    return 0;
+}
