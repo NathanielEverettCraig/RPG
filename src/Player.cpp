098e28da57656bf6f49d16bc8d0760cab7dfe05b
EricWRogers <ericwilliamrogers@gmail.com>
2026-02-26 13:27:32 -0600

    Player Move

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,13 +1,57 @@
 #include "Player.hpp"
+#include "Room.hpp"
 #include "fogpi/io.hpp"
 
 void Player::Start(Vec2 _pos) {
     m_character = 'P';
     m_position = _pos;
-
-    m_keyCount = 5;
 }
 
 void Player::Update() {
-    while(request_char("hit w to continue: ") != 'w') {}
+    //while(request_char("hit w to continue: ") != 'w') {}
+
+    char directionInput;
+
+    do {
+        directionInput = request_char("wasd and Enter to move");
+    } while (directionInput != 'w' &&
+             directionInput != 'a' &&
+             directionInput != 's' &&
+             directionInput != 'd');
+    
+    Vec2 direction(0.0f);
+
+    switch (directionInput)
+    {
+    case 'w':
+        direction = {0.0f, -1.0f};
+        break;
+    case 'a':
+        direction = {-1.0f, 0.0f};
+        break;
+    case 's':
+        direction = {0.0f, 1.0f};
+        break;
+    case 'd':
+        direction = {1.0f, 0.0f};
+        break;
+    default:
+        direction = {0.0f, 1.0f};
+        break;
+    }
+
+    Vec2 tryPos = m_position + direction;
+
+    if (room->GetLocation(tryPos) == 'K') {
+        m_keyCount++;
+        room->ClearLocation(tryPos);
+    }
+
+    if (room->GetLocation(tryPos) == ' ') {
+        m_position = tryPos;
+    }
+
+    if (room->GetLocation(tryPos) == 'D') {
+        room->OpenDoor(tryPos);
+    }
 }
